58d88ac115e7dce2154f390a8165818ba5ee04be
Jyotirmoy Barman <64324524+jyotirmoydotdev@users.noreply.github.com>
2023-03-11 19:57:30 +0530

    Deployed

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 //import logo from './logo.svg';
 import './App.css';
+import Header from './Component/header';
+import Card  from "./Component/card";
+import TextForm from "./Component/textform"
+import About from "./Component/about";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-    </nav>
-    
-  </>
+    <Router>
+      <Header />
+      <div className="container">
+
+        <Routes>
+          <Route path="/" element={<TextForm />} />
+          <Route path="about/*" element={<About />} />
+        </Routes>
+
+      </div>
+    </Router>
+    </>
   );
 }
 
